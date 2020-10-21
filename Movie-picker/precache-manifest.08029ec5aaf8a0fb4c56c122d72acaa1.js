b969e086778ccb0d775d559ce7b4ee0531214803
ugo-h <s_per00@outlook.com>
2020-10-21 22:28:36 +0300

    movie-picker added

---
+++ Movie-picker/precache-manifest.08029ec5aaf8a0fb4c56c122d72acaa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c65e81493397cefab9b93bc3f9242f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a4f08e3b1478f70d0ab",
+    "url": "./static/css/main.b27c2f75.chunk.css"
+  },
+  {
+    "revision": "6fb6601f4fe1b7d94cf1",
+    "url": "./static/js/2.27de31af.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.27de31af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4f08e3b1478f70d0ab",
+    "url": "./static/js/main.cd83af9f.chunk.js"
+  },
+  {
+    "revision": "601041587eaa57f338e2",
+    "url": "./static/js/runtime-main.f4033bb9.js"
+  },
+  {
+    "revision": "35ed46d9741d1cfbbe8471dd87e900fd",
+    "url": "./static/media/bodoni.35ed46d9.ttf"
+  }
+]);
