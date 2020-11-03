a947a00cf82d3dd517dca9bd327eaf9fcf77f5af
ugo-h <s_per00@outlook.com>
2020-11-03 17:03:28 +0300

    added new todo-app

---
+++ Todo-app/precache-manifest.d11638b57e7b92df6365036c7caf6c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4706c3abce69c2e67d5f35c9dbf75d6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "948468edb4b88b913851",
+    "url": "./static/css/main.17c8ac83.chunk.css"
+  },
+  {
+    "revision": "f7899fc8243233444fdd",
+    "url": "./static/js/2.a72cb99a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a72cb99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948468edb4b88b913851",
+    "url": "./static/js/main.da5a3cb2.chunk.js"
+  },
+  {
+    "revision": "37807aa12135f3f0df06",
+    "url": "./static/js/runtime-main.f6a946e7.js"
+  }
+]);
